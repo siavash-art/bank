f8ec6714e0af0ed97be26753352fbcd53d2f1c4e
Siyovush <siyovush@go.alif.academy>
2020-09-26 01:57:05 +0500

    edit types

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/siavash-art/bank
+module github.com/siavash-art/bank/v2
 
 go 1.15
--- pkg/types/types.go
+++ pkg/types/types.go
@@ -6,9 +6,20 @@ type Money int64
 //Category of payments categories
 type Category string
 
+//Status payment status
+type Status string
+
+//Status categories
+const (
+	StatusOk Status = "OK"
+	StatusFail Status = "FAIL"
+	StatusInProgress Status = "INPROGRESS"
+)
+
 //Payment struct
 type Payment struct {
 	ID       int
 	Amount   Money
 	Category Category
+	Status Status
 }
